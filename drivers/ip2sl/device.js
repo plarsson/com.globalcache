97836054559bad78a39e0da170e89f13073a7dd5
pln <peter@pln.se>
2018-10-07 12:07:41 +0200

    fix for serial

--- drivers/ip2sl/device.js
+++ drivers/ip2sl/device.js
@@ -17,9 +17,16 @@ class ITachIP2IRDevice extends ITachDevice {
 
   async executeCommand (args) {
     var mapping = super.getJsonConfig('serial')
-    const serialConfig = mapping.baudrate + ',' + mapping.flowcontrol + ',' + mapping.parity
     const cmdName = args.serialcmd.name
     const connectorAddress = args.connectoraddress.name
+
+    const header = []
+    header.push('set_SERIAL')
+    header.push(mapping.baudrate)
+    header.push(mapping.flowcontrol)
+    header.push(mapping.parity)
+    const serialConfig = header.join()
+
     const cmd = mapping.cmds.find(cmd => cmd.name === cmdName)
     this._sendSerial(connectorAddress, serialConfig, cmd.value)
   }
@@ -27,20 +34,23 @@ class ITachIP2IRDevice extends ITachDevice {
   _sendSerial (connectorAddress, serialConfig, payload) {
     const configClient = new net.Socket()
     configClient.connect(this._port, this._deviceData.ip, function () {
+      console.log('sending config ' + serialConfig)
       configClient.write(serialConfig + '\r')
     })
 
     configClient.on('data', function (data) {
       configClient.destroy()
-    })
 
-    const payloadClient = new net.Socket()
-    payloadClient.connect(this._serialPort, this._deviceData.ip, function () {
-      payloadClient.write(payload)
-    })
+      const payloadClient = new net.Socket()
+      // TODO assign serial port according to module pos
+      payloadClient.connect(this._serialPort, this._deviceData.ip, function () {
+        console.log('sending payload ' + payload)
+        payloadClient.write(payload)
+      })
 
-    payloadClient.on('data', function (data) {
-      payloadClient.destroy()
+      payloadClient.on('data', function (data) {
+        payloadClient.destroy()
+      })
     })
   }
 }
